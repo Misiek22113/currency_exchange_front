bf82effc106bf38645ec9d6aa0f165d33224af45
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-03-08 16:59:08 +0100

    add feddback

--- src/components/login-dialog/LogInDialog.tsx
+++ src/components/login-dialog/LogInDialog.tsx
@@ -1,74 +1,55 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-} from "@mui/material";
-import { LoginContext } from "../home-page/LoginContext";
-import { useContext, useState } from "react";
-import axios from "axios";
+import { Box, Dialog, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+import LoginPanel from "./LoginPanel";
 
 export interface LogInDialogProps {
   isOpen: boolean;
   onClose: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 const LoginDialog = ({ isOpen, onClose }: LogInDialogProps) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const loginContext = useContext(LoginContext);
+  const [tabIndex, setTabIndex] = useState(0);
 
   const handleOnClose = () => {
     onClose(false);
   };
 
-  const handleOnLogin = () => {
-    axios.post("http://localhost:4010/user/login", { username, password }).then(
-      (response) => {
-        if (response.data) {
-          loginContext.setIsLoggedIn(true);
-          loginContext.setUserId(response.data.userId);
-          onClose(false);
-        } else {
-          alert("Invalid username or password");
-        }
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabIndex(newValue);
+    event.preventDefault();
   };
 
   return (
     <Dialog onClose={handleOnClose} open={isOpen}>
-      <DialogTitle>Login</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="username"
-          label="Username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          fullWidth
-        />
-        <TextField
-          margin="dense"
-          id="password"
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleOnClose}>Cancel</Button>
-        <Button onClick={handleOnLogin}>Login</Button>
-      </DialogActions>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={tabIndex}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Log In" {...a11yProps(0)} />
+          <Tab label="Sign Up" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <LoginPanel
+        value={tabIndex}
+        index={0}
+        variant="login"
+        onClose={onClose}
+      />
+      <LoginPanel
+        value={tabIndex}
+        index={1}
+        variant="signup"
+        onClose={onClose}
+      />
     </Dialog>
   );
 };
---
+++ src/components/login-dialog/LoginPanel.tsx
@@ -0,0 +1,159 @@
+import {
+  Alert,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Snackbar,
+  TextField,
+} from "@mui/material";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { LoginContext } from "../home-page/LoginContext";
+
+interface TabPanelProps {
+  index: number;
+  value: number;
+  variant: "login" | "signup";
+  onClose: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginPanel = (props: TabPanelProps) => {
+  const { value, index, variant, onClose } = props;
+  const loginContext = useContext(LoginContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [open, setOpen] = useState(false);
+  const [signUpError, setSignUpError] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+
+  const handleOnClose = () => {
+    onClose(false);
+  };
+
+  const handleCloseToast = () => {
+    setOpen(false);
+  };
+
+  const handleOnLogin = () => {
+    axios.post("http://localhost:4010/user/login", { username, password }).then(
+      (response) => {
+        if (response.data) {
+          loginContext.setIsLoggedIn(true);
+          loginContext.setUserId(response.data.userId);
+          onClose(false);
+          setLoginError(false);
+        } else {
+          setLoginError(() => true);
+          alert("Invalid username or password");
+        }
+      },
+      (error) => {
+        setLoginError(() => true);
+        console.error(error);
+      }
+    );
+  };
+
+  const handleOnSignUp = () => {
+    axios
+      .post("http://localhost:4010/user/signup", { username, password })
+      .then(
+        (response) => {
+          if (response.data) {
+            setSignUpError(false);
+            onClose(false);
+          } else {
+            setSignUpError(() => true);
+          }
+        },
+        (error) => {
+          setSignUpError(() => true);
+          console.error(error);
+        }
+      );
+  };
+
+  return (
+    <div hidden={value !== index}>
+      <DialogTitle>{variant === "login" ? "Log Un" : "Sign Up"}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="username"
+          label="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          id="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleOnClose}>Cancel</Button>
+        <Button onClick={variant === "login" ? handleOnLogin : handleOnSignUp}>
+          {variant === "login" ? "Log In" : "Sign Up"}
+        </Button>
+      </DialogActions>
+      {variant === "signup" ? (
+        <Snackbar
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleCloseToast}
+        >
+          <Alert
+            onClose={handleCloseToast}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Successfully signed up!
+          </Alert>
+        </Snackbar>
+      ) : null}
+      {signUpError ? (
+        <Snackbar
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleCloseToast}
+        >
+          <Alert
+            onClose={handleCloseToast}
+            severity="error"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Error signing up
+          </Alert>
+        </Snackbar>
+      ) : null}
+      {loginError ? (
+        <Snackbar
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleCloseToast}
+        >
+          <Alert
+            onClose={handleCloseToast}
+            severity="error"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Error logging in
+          </Alert>
+        </Snackbar>
+      ) : null}
+    </div>
+  );
+};
+
+export default LoginPanel;
